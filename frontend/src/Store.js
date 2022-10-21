d25a3d814f5ced074b29a5822843ec32ac815ac1
amit <amitgkpup@hotmail.com>
2022-10-21 18:47:01 +0530

    yygh

---
+++ frontend/src/Store.js
@@ -0,0 +1,20 @@
+import {createStore,combineReducers,applyMiddleware} from "redux";
+import Thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { productReducer } from "./reducers/productReducer";
+
+const reducer= combineReducers({
+    products:productReducer,
+});
+let initialState={
+
+};
+const middleware=[thunk];
+const store=createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,9 +1,22 @@
 import React, { Fragment } from 'react';
 import {CgMouse} from "react-icons/cg";
+import MetaData from '../layout/Header/MetaData';
 import "./Home.css";
+//import "./Product"
+import Product from './Product';
+
+const product={
+    name:"abc",
+    images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
+    price:"899",
+    _id:"xyz",
+};
 const Home = () => {
   return (
+
+
    <Fragment>
+   <MetaData title="Ecommerce"/>
     <div  className='banner'>
 <p>Welcome to Ecommerce</p>
 <h1>FIND AMAZING PRODUCTS BELOW</h1>
@@ -14,6 +27,18 @@ scroll
 <CgMouse/>
     </button>
 </a>
+    </div>
+    <h2 className='homeHeading'>Featured Products</h2>
+    <div className='container' id='container'>
+      <Product product={product}/>
+      <Product product={product}/>
+      <Product product={product}/>
+      <Product product={product}/>
+
+      <Product product={product}/>
+      <Product product={product}/>
+      <Product product={product}/>
+      <Product product={product}/>
     </div>
    </Fragment>
   )
